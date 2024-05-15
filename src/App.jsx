d3c4e9e8dac1ee0304e65a11d24ce019f1a591c9
Wiktor <lolxdyoloelo@gmail.com>
2024-05-15 22:55:26 +0200

    sdfsdfsdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-const quote_list = 
-[["Ludzie śpią", "Moi rodzice są nubkami", "eeeem"], // michaj
-["Ej bez kitu", "To nieźle..."], // pio
-["NULL"], // wiktor
-["NULL"], // klubiś
-["NULL"], // mbycz
-["NULL"], // kubibex
-["Jogurt gradientowy istnieje"], // niedzwiedz
-["Nie rzucaj mięsem"]] // martyna
-let random_quote
-var bools = [0, 0, 0, 0, 0, 0, 0]
+const quote_list = [
+  ["Ludzie śpią", "Moi rodzice są nubkami", "eeeem"], // michaj
+  ["Ej bez kitu", "To nieźle..."], // pio
+  ["asd"], // wiktor
+  ["NUssLL"], // klubiś
+  ["NaassdULL"], // mbycz
+  ["NULssssL"], // kubibex
+  ["Jogurt gradientowy istnieje"], // niedzwiedz
+  ["Nie rzucaj mięsem"],
+]; // martyna
+let random_quote;
+var bools = [0, 0, 0, 0, 0, 0, 0];
 
 function App() {
   return (
@@ -21,21 +22,39 @@ function App() {
       <div className="main">
         <h1>KALAFARD</h1>
         <div className="card"></div>
-        <button onClick={() => {
-          GetRandomQuote();
-          console.log(random_quote)
-        }}>losu losu</button>
+        <LosuComponent />
         <p>Kalafard by WitroxHunter & 7even</p>
       </div>
     </>
   );
 }
 
-function GetRandomQuote() {
-  var chosen_row = Math.floor(Math.random() * 6)
-  random_quote = quote_list[chosen_row][Math.floor(Math.random() * (quote_list[chosen_row].length))]
-  bools[chosen_row] = 1
+function LosuComponent() {
+  const [currentQuote, setCurrentQuote] = useState("");
+
+  function GetRandomQuote() {
+    var chosen_row = Math.floor(Math.random() * 6);
+    random_quote =
+      quote_list[chosen_row][
+        Math.floor(Math.random() * quote_list[chosen_row].length)
+      ];
+    bools[chosen_row] = 1;
+    setCurrentQuote(random_quote);
+  }
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          GetRandomQuote();
+          console.log(random_quote);
+        }}
+      >
+        Losuj quote
+      </button>
+      {currentQuote}
+    </>
+  );
 }
 
-
-export default App
+export default App;
