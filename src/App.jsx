d5425db4439cf5cdd04abaa62504a2f1138fb4bf
7evenPodloga <155751018+7evenPodloga@users.noreply.github.com>
2024-05-15 20:02:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,26 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 const quote_list = ["ludzie śpią", "gradientowy jogurt istnieje", "eeeem", "lubie umięśnionych mężczyzn"];
+let random_quote
 
 function App() {
-
   return (
     <>
       <div className="main">
         <h1>KALAFARD</h1>
         <div className="card"></div>
         <button onClick={() => {
+          GetRandomQuote();
+          console.log(random_quote)
         }}>losu losu</button>
-        <p>Kalafard by WitroxHunter & 7even {GetRandomQuote()}</p>
+        <p>Kalafard by WitroxHunter & 7even</p>
       </div>
     </>
   );
 }
 
 function GetRandomQuote() {
-  let random_quote = quote_list[Math.floor(Math.random() * (quote_list.length))]
-  return (random_quote)
+  random_quote = quote_list[Math.floor(Math.random() * (quote_list.length))]
 }
 
 export default App;
